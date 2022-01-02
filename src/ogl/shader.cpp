5f18558c8c1562453b8b68e17daf9de68befb3ea
JaimieVos <jaimie.vos@outlook.com>
2022-01-02 14:39:07 +0100

    Added methods to set uniforms in the Shader class

--- src/ogl/shader.cpp
+++ src/ogl/shader.cpp
@@ -5,6 +5,8 @@
 #include "util/logger.h"
 #include "io/file_stream.h"
 
+#include <glm/gtc/type_ptr.hpp>
+
 Shader::Shader(const char* vertexPath, const char* fragmentPath)
 {
 	std::string vertexCode = file_stream::readFile(vertexPath);
@@ -52,6 +54,111 @@ void Shader::bind() const
 void Shader::unbind() const
 {
 	glUseProgram(0);
+}
+
+void Shader::setInt(const char* name, const int value) const
+{
+	glUniform1i(getLocation(name), value);
+}
+
+void Shader::set2Int(const char* name, const int x, const int y) const
+{
+	glUniform2i(getLocation(name), x, y);
+}
+
+void Shader::set3Int(const char* name, const int x, const int y, const int z) const
+{
+	glUniform3i(getLocation(name), x, y, z);
+}
+
+void Shader::set4Int(const char* name, const int x, const int y, const int z, const int w) const
+{
+	glUniform4i(getLocation(name), x, y, z, w);
+}
+
+void Shader::set2iVector(const char* name, const glm::ivec2& value) const
+{
+	glUniform2iv(getLocation(name), 1, glm::value_ptr(value));
+}
+
+void Shader::set3iVector(const char* name, const glm::ivec3& value) const
+{
+	glUniform3iv(getLocation(name), 1, glm::value_ptr(value));
+}
+
+void Shader::set4iVector(const char* name, const glm::ivec4& value) const
+{
+	glUniform4iv(getLocation(name), 1, glm::value_ptr(value));
+}
+
+void Shader::setUInt(const char* name, const unsigned int value) const
+{
+	glUniform1ui(getLocation(name), value);
+}
+
+void Shader::set2UInt(const char* name, const unsigned int x, const unsigned int y) const
+{
+	glUniform2ui(getLocation(name), x, y);
+}
+
+void Shader::set3UInt(const char* name, const unsigned int x, const unsigned int y, const unsigned int z) const
+{
+	glUniform3ui(getLocation(name), x, y, z);
+}
+
+void Shader::set4UInt(const char* name, const unsigned int x, const unsigned int y, const unsigned int z, const unsigned int w) const
+{
+	glUniform4ui(getLocation(name), x, y, z, w);
+}
+
+void Shader::setFloat(const char* name, const float value) const
+{
+	glUniform1f(getLocation(name), value);
+}
+
+void Shader::set2Float(const char* name, const float x, const float y) const
+{
+	glUniform2f(getLocation(name), x, y);
+}
+
+void Shader::set3Float(const char* name, const float x, const float y, const float z) const
+{
+	glUniform3f(getLocation(name), x, y, z);
+}
+
+void Shader::set4Float(const char* name, const float x, const float y, const float z, const float w) const
+{
+	glUniform4f(getLocation(name), x, y, z, w);
+}
+
+void Shader::set2fVector(const char* name, const glm::vec2& value) const
+{
+	glUniform2fv(getLocation(name), 1, glm::value_ptr(value));
+}
+
+void Shader::set3fVector(const char* name, const glm::vec3& value) const
+{
+	glUniform3fv(getLocation(name), 1, glm::value_ptr(value));
+}
+
+void Shader::set4fVector(const char* name, const glm::vec4& value) const
+{
+	glUniform4fv(getLocation(name), 1, glm::value_ptr(value));
+}
+
+void Shader::setMatrix2f(const char* name, const glm::mat2& value) const
+{
+	glUniformMatrix2fv(getLocation(name), 1, false, glm::value_ptr(value));
+}
+
+void Shader::setMatrix3f(const char* name, const glm::mat3& value) const
+{
+	glUniformMatrix3fv(getLocation(name), 1, false, glm::value_ptr(value));
+}
+
+void Shader::setMatrix4f(const char* name, const glm::mat4& value) const
+{
+	glUniformMatrix4fv(getLocation(name), 1, false, glm::value_ptr(value));
 }
 
 int Shader::getLocation(const std::string& name) const
